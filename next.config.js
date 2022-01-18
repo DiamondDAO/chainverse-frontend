3e8cea362395f50daa070485cea62de744d2fd94
Alex Keating <keating.dev@protonmail.com>
2022-01-18 10:29:16 -0500

    Add a print

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+console.log("API_URL");
+console.log(`${process.env.API_URL}`);
 module.exports = {
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   reactStrictMode: true,
@@ -6,7 +8,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: `http://${process.env.API_URL}/:path*`,
+        destination: `http://localhost:5000/:path*`,
       },
     ];
   },
