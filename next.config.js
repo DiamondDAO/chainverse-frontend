c801e85b383644da49bd9f98eb2396ccf4c601bc
Keating <keating.dev@protonmail.com>
2022-02-07 17:00:47 -0500

    Update url

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const dns = require("dns");
-console.log(process.env.API_URL);
-console.log(dns.lookup(process.env.API_URL));
 module.exports = {
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   reactStrictMode: true,
   async rewrites() {
+    let address = null;
+    console.log(process.env.API_URL);
+    console.log(
+      dns.lookup(process.env.API_URL, (err, address, family) => {
+        console.log("address: %j family: IPv%s", address, family);
+        address = address;
+      })
+    );
+    // console.log(dns.lookup(process.env.API_URL));
+    const url = address ? `http://${address}` : process.env.API_URL;
+    console.log(url);
     return [
       {
         source: "/api/graphql",
-        destination: `${process.env.API_URL}`,
+        destination: `${url}`,
       },
     ];
   },
